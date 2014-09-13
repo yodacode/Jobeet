b0f28eb7618d73df142cd34b6c7f4268f3d2ccaf
yodacode <benjamin.devaublanc@gmail.com>
2014-09-13 12:01:06 +0200

    Day 8 les tests unitaires

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -12,27 +12,42 @@
  */
 class JobeetCategory extends BaseJobeetCategory
 {
-	public function getActiveJobs($max = 10)
-	{
-	  $q = Doctrine_Query::create()
-	    ->from('JobeetJob j')
-	    ->where('j.category_id = ?', $this->getId())
-	    ->limit($max);
+	// public function getActiveJobs($max = 10)
+	// {
+	//   $q = Doctrine_Query::create()
+	//     ->from('JobeetJob j')
+	//     ->where('j.category_id = ?', $this->getId())
+	//     ->limit($max);
 	 
-	  return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
-	}
-
-	public function getSlug()
-	{
-	  return Jobeet::slugify($this->getName());
-	}
+	//   return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
+	// }	
 
 	public function countActiveJobs()
+	{
+	  return $this->getActiveJobsQuery()->count();
+	}
+
+	public function getActiveJobs($max = 10) 
+	{ 
+		$q = $this->getActiveJobsQuery()->limit($max);
+		return $q->execute();
+	}
+
+	// public function countActiveJobs()
+	// {
+	//   $q = Doctrine_Query::create()
+	//     ->from('JobeetJob j')
+	//     ->where('j.category_id = ?', $this->getId());
+	 
+	//   return Doctrine_Core::getTable('JobeetJob')->countActiveJobs($q);
+	// }
+
+	public function getActiveJobsQuery()
 	{
 	  $q = Doctrine_Query::create()
 	    ->from('JobeetJob j')
 	    ->where('j.category_id = ?', $this->getId());
 	 
-	  return Doctrine_Core::getTable('JobeetJob')->countActiveJobs($q);
+	  return Doctrine_Core::getTable('JobeetJob')->addActiveJobsQuery($q);
 	}
 }
