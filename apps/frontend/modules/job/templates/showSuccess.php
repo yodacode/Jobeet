550255d5bedeef8081f19fc871c14c82da91499d
yodacode <benjamin.devaublanc@gmail.com>
2014-09-16 13:18:07 +0200

    Day 10 formulaire

--- apps/frontend/modules/job/templates/showSuccess.php
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -2,6 +2,11 @@
 <?php slot('title') ?>
   <?php echo sprintf('%s is looking for a %s', $job->getCompany(), $job->getPosition()) ?>
 <?php end_slot(); ?>
+
+<?php if ($sf_request->getParameter('token') == $job->getToken()): ?>
+  <?php include_partial('job/admin', array('job' => $job)) ?>
+<?php endif ?>
+
 <div id="job">
   <h1><?php echo $job->getCompany() ?></h1>
   <h2><?php echo $job->getLocation() ?></h2>
@@ -13,8 +18,7 @@
   <?php if ($job->getLogo()): ?>
     <div class="logo">
       <a href="<?php echo $job->getUrl() ?>">
-        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
-          alt="<?php echo $job->getCompany() ?> logo" />
+        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>" alt="<?php echo $job->getCompany() ?> logo" />
       </a>
     </div>
   <?php endif; ?>
@@ -32,8 +36,6 @@
   </div>
  
   <div style="padding: 20px 0">
-    <a href="<?php echo url_for('job/edit?id='.$job->getId()) ?>">
-      Edit
-    </a>
+    <a href="<?php echo url_for('job_edit', $job) ?>">Edit</a>
   </div>
 </div>
